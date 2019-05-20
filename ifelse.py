8283ded3a15ac650937d9e71fce0df767c1433ea
pushpinder <pushpinders555@gmail.com>
2019-05-20 17:59:01 +0530

    new file

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+first_name = input('what is your name? ')
+print('Hello', first_name)
+if first_name == 'romi':
+    print('Nice to meet you {}!'.format(first_name))
+elif first_name == 'garry':
+    print('it is pleasure to meet you', first_name)
+else:
+    age = int(input('How old are u'))
+    if age <= 6:
+        print('You are older to join us {}!'.format(age))
+print('Good luck bro')
